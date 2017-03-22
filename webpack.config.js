9b9b46ff2d60c8eb1792f3c142edbb59e9b47d03
Vidsneezes <moraleszez@gmail.com>
2017-03-22 16:29:42 -0600

    modified webpack config js to use react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
+var webpack = require('webpack');
+
 module.exports = {
     entry: [
+        'webpack-dev-server/client?http://local:8080',
+        'webpack/hot/only-dev-server',
         './src/index.js'
     ],
     modulue: {
         loaders: [{
             test: /\.jsx?$/,
             exclude: /node_modules/,
-            loader: 'babel-loader'
+            loader: 'react-hot-loader!babel-loader'
         }]
     },
-    resolve:{
-        extensions: ['','.js','.jsx']
+    resolve: {
+        extensions: ['', '.js', '.jsx']
     },
     output: {
         path: __dirname + '/dist/',
@@ -18,6 +22,10 @@ module.exports = {
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './dist/'
-    }
+        contentBase: './dist/',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
 };
