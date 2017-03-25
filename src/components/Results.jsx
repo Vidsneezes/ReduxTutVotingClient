7b00a7e61466f0ba20e72b3de759d8eb061c56da
Vidsneezes <moraleszez@gmail.com>
2017-03-25 11:00:37 -0600

    adds results test, modifies results jsx to account for tally, relfects changes in app jsx

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -6,12 +6,22 @@ class Results extends React.PureComponent {
         return this.props.pair || [];
     }
 
+    getVotes(entry) {
+        if (this.props.tally && this.props.tally.has(entry)) {
+            return this.props.tally.get(entry);
+        }
+        return 0;
+    }
+
     render() {
         return (
           <div className="Results">
             {this.getPair().map(entry =>
               <div key={entry} className="entry">
                 <h1>{entry}</h1>
+                <div className="voteCounte">
+                  {this.getVotes(entry)}
+                </div>
               </div>)}
           </div>
         );
