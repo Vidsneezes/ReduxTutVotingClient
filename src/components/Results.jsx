ae4564b5882f556b45d6d973c3067579da6c2dca
Vidsneezes <moraleszez@gmail.com>
2017-03-25 10:45:06 -0600

    added render logic for results

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 
 class Results extends React.PureComponent {
+
+    getPair() {
+        return this.props.pair || [];
+    }
+
     render() {
-        return <div>Hello World!</div>;
+        return (
+          <div className="Results">
+            {this.getPair().map(entry =>
+              <div key={entry} className="entry">
+                <h1>{entry}</h1>
+              </div>)}
+          </div>
+        );
     }
 }
 
