587f8019b718210a592cddbdffeece2696224c13
Vidsneezes <moraleszez@gmail.com>
2017-03-25 11:16:27 -0600

    addes new conditional for when a winner is found, implements test as 'renders the winner when there is one

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Winner from './Winner';
 
 class Results extends React.PureComponent {
 
@@ -15,22 +16,24 @@ class Results extends React.PureComponent {
 
     render() {
         return (
-          <div className="results">
-            <div className="tally">
-              {this.getPair().map(entry =>
-                <div key={entry} className="entry">
-                  <h1>{entry}</h1>
-                  <div className="voteCount">
-                    {this.getVotes(entry)}
-                  </div>
-                </div>)}
+          this.props.winner ?
+            <Winner ref="winner" winner={this.props.winner} /> :
+            <div className="results">
+              <div className="tally">
+                {this.getPair().map(entry =>
+                  <div key={entry} className="entry">
+                    <h1>{entry}</h1>
+                    <div className="voteCount">
+                      {this.getVotes(entry)}
+                    </div>
+                  </div>)}
+              </div>
+              <div className="management">
+                <button ref="next" className="next" onClick={this.props.next}>
+                  Next
+                </button>
+              </div>
             </div>
-            <div className="management">
-              <button ref="next" className="next" onClick={this.props.next}>
-                Next
-              </button>
-            </div>
-          </div>
         );
     }
 }
