42c598a5b88a374938cb1c263a45d86920f43214
Vidsneezes <moraleszez@gmail.com>
2017-03-27 11:01:35 -0600

    finished app, added next action to actionCreators, relfect change in Results jsx to us next action type

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Winner from './Winner';
+import * as actionCreators from '../action_creators';
 
 export class Results extends React.PureComponent {
 
@@ -47,4 +48,4 @@ function mapStateToProps(state) {
     };
 }
 
-export const ResultsContainer = connect(mapStateToProps)(Results);
+export const ResultsContainer = connect(mapStateToProps, actionCreators)(Results);
