954bb9030e79d38a09e4bc05f336c164e28b8fa2
Vidsneezes <moraleszez@gmail.com>
2017-03-27 10:48:04 -0600

    added conditional checking in remoteactionmiddleware to check for remote, reflected changes in actioncreators in vote to add a remote true

--- src/remote_action_middleware.js
+++ src/remote_action_middleware.js
@@ -1,4 +1,6 @@
 export default socket => store => next => (action) => {
-    console.log('in middleware', action);
+    if (action.meta && action.remote) {
+        socket.emit('action', action);
+    }
     return next(action);
 };
