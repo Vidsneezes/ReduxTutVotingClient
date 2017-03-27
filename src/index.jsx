7a5b37a500d6a8143a3f66572e00245e78587162
Vidsneezes <moraleszez@gmail.com>
2017-03-27 10:43:41 -0600

    modified remote_action_middleware to include socket , on index js changed applymiddleware parameter to include the new socket

--- src/index.jsx
+++ src/index.jsx
@@ -11,15 +11,15 @@ import remoteActionMiddleware from './remote_action_middleware';
 import { VotingContainer } from './components/Voting';
 import { ResultsContainer } from './components/Results';
 
-const createStoreWithMiddleware = applyMiddleware(
-  remoteActionMiddleware
-)(createStore);
-const store = createStoreWithMiddleware(reducer);
-
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state =>
   store.dispatch(setState(state))
 );
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
 
 const routes = (
   <Route component={App}>
